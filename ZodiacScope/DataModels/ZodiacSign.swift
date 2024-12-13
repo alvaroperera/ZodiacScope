7bcef91877f5ce804cf185529de8f4bf6fbaca82
Álvaro Perera <alvprodz@gmail.com>
2024-12-13 20:26:35 +0100

    20241213_APR_ZodiacScopeNowUsesHoroscopeAPIToGetPredictions

--- ZodiacScope/DataModels/ZodiacSign.swift
+++ ZodiacScope/DataModels/ZodiacSign.swift
@@ -7,28 +7,40 @@
 
 import Foundation
 
-struct ZodiacSign {
+struct ZodiacSign: Codable {
     let id: String
     let name: String
     let iconImage: String
     let dates: String
+    let todayPrediction: String
     
     static func getAllZodiacSigns() -> [ZodiacSign] {
         let zodiacSignsList: [ZodiacSign] =
         [
-            ZodiacSign(id: "aries", name: "Aries", iconImage: "aries", dates: "March 21 - April 19"),
-            ZodiacSign(id: "taurus", name: "Taurus", iconImage: "taurus", dates: "April 20 - May 20"),
-            ZodiacSign(id: "gemini", name: "Gemini", iconImage: "gemini", dates: "May 21 - June 20"),
-            ZodiacSign(id: "cancer", name: "Cancer", iconImage: "cancer", dates: "June 21 - July 22"),
-            ZodiacSign(id: "leo", name: "Leo", iconImage: "leo", dates: "July 23 - August 22"),
-            ZodiacSign(id: "virgo", name: "Virgo", iconImage: "virgo", dates: "August 23 - September 22"),
-            ZodiacSign(id: "libra", name: "Libra", iconImage: "libra", dates: "September 23 - October 22"),
-            ZodiacSign(id: "scorpio", name: "Scorpio", iconImage: "scorpio", dates: "October 23 - November 21"),
-            ZodiacSign(id: "sagittarius", name: "Sagittarius", iconImage: "sagittarius", dates: "November 22 - December 21"),
-            ZodiacSign(id: "capricorn", name: "Capricorn", iconImage: "capricorn", dates: "December 22 - January 21"),
-            ZodiacSign(id: "aquarius", name: "Aquarius", iconImage: "aquarius", dates: "January 22 - February 19"),
-            ZodiacSign(id: "pisces", name: "Pisces", iconImage: "pisces", dates: "February 20 - March 20")
+            ZodiacSign(id: "aries", name: "Aries", iconImage: "aries", dates: "March 21 - April 19", todayPrediction: ""),
+            ZodiacSign(id: "taurus", name: "Taurus", iconImage: "taurus", dates: "April 20 - May 20", todayPrediction: ""),
+            ZodiacSign(id: "gemini", name: "Gemini", iconImage: "gemini", dates: "May 21 - June 20", todayPrediction: ""),
+            ZodiacSign(id: "cancer", name: "Cancer", iconImage: "cancer", dates: "June 21 - July 22", todayPrediction: ""),
+            ZodiacSign(id: "leo", name: "Leo", iconImage: "leo", dates: "July 23 - August 22", todayPrediction: ""),
+            ZodiacSign(id: "virgo", name: "Virgo", iconImage: "virgo", dates: "August 23 - September 22", todayPrediction: ""),
+            ZodiacSign(id: "libra", name: "Libra", iconImage: "libra", dates: "September 23 - October 22", todayPrediction: ""),
+            ZodiacSign(id: "scorpio", name: "Scorpio", iconImage: "scorpio", dates: "October 23 - November 21", todayPrediction: ""),
+            ZodiacSign(id: "sagittarius", name: "Sagittarius", iconImage: "sagittarius", dates: "November 22 - December 21", todayPrediction: ""),
+            ZodiacSign(id: "capricorn", name: "Capricorn", iconImage: "capricorn", dates: "December 22 - January 21", todayPrediction: ""),
+            ZodiacSign(id: "aquarius", name: "Aquarius", iconImage: "aquarius", dates: "January 22 - February 19", todayPrediction: ""),
+            ZodiacSign(id: "pisces", name: "Pisces", iconImage: "pisces", dates: "February 20 - March 20", todayPrediction: "")
         ]
         return zodiacSignsList
     }
+}
+
+struct HoroscopeAPIResponse: Codable {
+    let data: HoroscopeAPIResponseData
+    let status: Int
+    let success: Bool
+}
+
+struct HoroscopeAPIResponseData: Codable {
+    let date: String
+    let horoscope_data: String
 }
--- ZodiacScope/ViewControllers/ZodiacSignDetailViewController.swift
+++ ZodiacScope/ViewControllers/ZodiacSignDetailViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 class ZodiacSignDetailViewController: UIViewController{
     
     @IBOutlet weak var zodiacSignIMG: UIImageView!
+    @IBOutlet weak var zodiacSignNameLabel: UILabel!
+    
+    @IBOutlet weak var predictionLabel: UITextView!
     var zodiacSign: ZodiacSign?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.}
         loadViewItems()
+        
+        func loadViewItems(){
+            zodiacSignIMG.image = UIImage(named: zodiacSign!.iconImage)
+            zodiacSignNameLabel.text = zodiacSign!.name
+            self.fetchPrediction { result in
+                if case .success(let horoscopeAPIResponse) = result {
+                    DispatchQueue.main.async {
+                        self.predictionLabel.text = horoscopeAPIResponse.data.horoscope_data
+                    }
+                }
+            }
+        }
     }
     
-    func loadViewItems(){
-        zodiacSignIMG.image = UIImage(named: zodiacSign!.iconImage)
+    func fetchPrediction(completion: @escaping (Result<HoroscopeAPIResponse, Error>) -> Void) {
+
+        guard let url = URL(string: "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=\(self.zodiacSign!.id)&day=TODAY")
+        else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return completion(.failure(error!))
+            }
+            let decoder = JSONDecoder()
+            do {
+                let horoscopeAPIResult = try decoder.decode(HoroscopeAPIResponse.self, from: data)
+                completion(.success(horoscopeAPIResult))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        .resume()
+
     }
 }
